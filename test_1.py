02ce1a5b64e713052e434fcb2d19977627e905d5
bvaldez25 <bvaldez25@mail.fresnostate.edu>
2020-11-12 16:48:23 -0800

    finshed structured-english

---
+++ test_1.py
@@ -0,0 +1,14 @@
+"""
+The program is trying to determine which payment option is better. 
+First option is $100 per day for 10 days. The second option is $1 dollar a day
+with it being doubled each day for 10 days. There will be two functions
+to calculate the pay rate. Function1 will calculate the amount for the first option,
+Function2 will calculate the amount for the second option.
+
+Function1 will output 100 * 10 days.
+Function2 will loop 10 times, with each time, doubling the amount and add the amount to the total.
+
+If the amount is equal, we output to the user “Option 1 and Option 2 pays the same”.
+If option1 is better, we output to the user “Option 1 is better”.
+If option2 is better, we output to the user “Option 2 is better”.
+"""
